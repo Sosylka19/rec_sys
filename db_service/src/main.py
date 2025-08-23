544dc8711122d639e1f3e8e81bb505f091dc9065
Sosylka19 <sashasasha192021@gmail.com>
2025-08-23 18:59:56 +0300

    trash

--- db_service/src/main.py
+++ db_service/src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 @app.get("/health_check")
 async def health_check():
+    logger.info("получен запрос db")
+    print("smth db")
     return {"status": "Db service started"}
--- ml_service/src/main.py
+++ ml_service/src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+import logging
 
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 @app.get("/health_check")
