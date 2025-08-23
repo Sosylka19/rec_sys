86bfdd7d783c6b7c24002ed55b309b120ae1d272
Sosylka19 <sashasasha192021@gmail.com>
2025-08-23 19:17:38 +0300

    bla

--- ml_service/src/main.py
+++ ml_service/src/main.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 app = FastAPI()
 
-@app.get("/health_check")
+@app.get("/health_check/")
 async def health_check():
     logger.info("получен запрос мл")
     print("smth ml")
