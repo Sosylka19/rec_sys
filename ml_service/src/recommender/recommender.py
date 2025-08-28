763054a058567b6468072fce5bd764102dd4987e
Sosylka19 <sashasasha192021@gmail.com>
2025-08-28 12:11:28 +0300

    upd: eda+baseline & ml kernel

--- ml_service/src/recommender/recommender.py
+++ ml_service/src/recommender/recommender.py
@@ -1,8 +1,8 @@
 from typing import List
+from ml_kernel import recommend_movies
 class Recommender():
     def __init__(self, film: str):
         self.film = film
 
     def recommend(self, recommendation: List[str]) -> str:
-        #Magic with model
-        return "Bla1 Bla2"
+        return ", ".join(recommend_movies(self.film))
